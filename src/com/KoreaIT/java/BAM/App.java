f68613faedf3d968f6c2fe2c9d63eb86180cadc7
shinsangsoo <sangsoo0070@naver.com>
2022-07-18 18:51:31 +0900

    로직 중복제거 시도 중

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -67,16 +67,7 @@ public class App {
 
 				int id = Integer.parseInt(cmdBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 없습니다\n", id);
@@ -154,6 +145,20 @@ public class App {
 
 		System.out.println("==프로그램 끝==");
 		sc.close();
+	}
+
+	private Article getArticleById(int id) {
+		
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+
+			if (article.id == id) {
+				Article foundArticle = article;
+				break;
+				
+			}
+		}
+		return null;
 	}
 
 	private void makeTestData() {
