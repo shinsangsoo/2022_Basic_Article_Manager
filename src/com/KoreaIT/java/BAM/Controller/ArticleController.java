c25b182cdf3a33b7b5a89faf7bca99304222653c
shinsangsoo <sangsoo0070@naver.com>
2022-07-25 18:20:36 +0900

    게시글의 작성자만 수정과 삭제가능 기능  구현

--- src/com/KoreaIT/java/BAM/Controller/ArticleController.java
+++ src/com/KoreaIT/java/BAM/Controller/ArticleController.java
@@ -165,6 +165,13 @@ public class ArticleController extends Controller {
 			System.out.printf("%d번 게시물은 없습니다\n", id);
 			return;
 		}
+		
+		if((loginedMember.id != foundArticle.memberId)) {
+			System.out.printf("권한이 없습니다\n");
+			return;
+			
+		} 
+		
 		System.out.printf("제목 : ");
 		String title = sc.nextLine();
 		System.out.printf("내용 : ");
@@ -172,10 +179,10 @@ public class ArticleController extends Controller {
 
 		foundArticle.title = title;
 		foundArticle.body = body;
-		
+			
 
 		System.out.printf("%d번 게시물을 수정했습니다\n", id);
-
+		
 		
 	}
 
@@ -189,14 +196,19 @@ public class ArticleController extends Controller {
 		
 		int id = Integer.parseInt(cmdBits[2]);
 
-		int foundIndex = getArticleIndexById(id);
+		Article foundArticle = getArticleById(id);
 
-		if (foundIndex == -1) {
+		if (foundArticle == null) {
 			System.out.printf("%d번 게시물은 없습니다\n", id);
 			return;
 		}
+		
+		if((loginedMember.id != foundArticle.memberId)) {
+			System.out.printf("권한이 없습니다\n");
+			return;
+		}
 
-		articles.remove(foundIndex);
+		articles.remove(foundArticle);
 		System.out.printf("%d번 게시물을 삭제했습니다\n", id);
 		
 	}
